2756e36715e45b6e89a766773e7ebc9ff7bbcba3
Dan Sebastian Costa <92059991+ArcaniteM@users.noreply.github.com>
2022-02-25 19:52:09 -0500

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.scss';
 export default function Footer() {
   return (
     <div className="Footer">
-          <a href="http://www.google.com" className='material-icons floating-btn'>add</a>
+          <a href="http://www.google.com" className='material-icons floating-btn'>+</a>
     </div>
   );
 }
